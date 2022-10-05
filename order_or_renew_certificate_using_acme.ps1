fe48d2d623a4ae83832b0eed06835cb37cd36360
Holger1968 <33520942+Holger1968@users.noreply.github.com>
2022-10-05 09:08:31 +0200

    Update order_or_renew_certificate_using_acme.ps1

--- order_or_renew_certificate_using_acme.ps1
+++ order_or_renew_certificate_using_acme.ps1
@@ -10,12 +10,9 @@ this runbook is (normally) installed through setup_azure_infrastructure_for_acme
 
 #>
 
-# create $ACMEDNSdomain in Azure public dns, it can have any name, f.e. acme.example.com; of course you must control (the apex) example.com
-# create ns entry for $acmedomain on the dns-servers for $apex ($apex might be on Azure DNS or not) and point it to Azure DNS Servers (4 servers, you are told them when creating $acmedomain in Azure DNS or you can list them in Azure DNS)
-#FIXME PAServer LE_PROD
-#FIXME in Doku erwähnen (readme.md), dass einfach neue Zertifikate hinzugefügt werden können (im Schedule!)
-#FIXME test von "check if certificate schon vorhanden, sonst neues"
-#FIXME output was gemacht wurde (new cert, renewal, key vault updates)
+#FIXME in Doku erwähnen (readme.md?), dass einfach neue Zertifikate hinzugefügt werden können (im Schedule!), aber CNAME nicht vergessen
+#doku als readme.md in GIT.swisscom.com
+
 #region params, vars, checks etc
 param (
   [Parameter(Mandatory = $true)]
@@ -27,36 +24,38 @@ param (
   [Parameter(Mandatory = $true)]
   [string]$ACMEDNSDomain
   )    
+$errorActionPreference = "Stop"
+$WarningPreference = 'SilentlyContinue'
+trap {$errmsg = ($_ | format-list * -force | out-string) ; "TRAP CALLED at $(Get-Date) : $errmsg" ; break}  # Error: $_ and Exception: $_.Exception.Message and 
 
-$PAServer = "LE_STAGE"
+# $GLOBAL:DebugPreference = "Continue"  # comment out in order to NOT get debug output
 $workingDirectory = Join-Path -Path $pwd -ChildPath "posh-acme"
 $poshcontainer = "posh-acme"
 $poshfile = "posh-acme.zip"
-$GLOBAL:DebugPreference="Continue"  # comment out in order to NOT get debug output
-
-$errorActionPreference = "Stop"
-trap {$errmsg = ($_ | format-list * -force | out-string) ; "TRAP CALLED : $errmsg" ; break}  # Error: $_ and Exception: $_.Exception.Message and 
 
 if (!(Get-AzAccessToken -ErrorAction SilentlyContinue)){
   if (!$env:AZUREPS_HOST_ENVIRONMENT) {
-    $az = Connect-AzAccount -TenantId 84d7ef22-1ddc-48ce-bf9b-0f099c1ebdf8 -Subscription 6933d5e6-880a-4d60-a474-35b1816d0d62 # Swisscom Azure Testlab
-  }      
-  else {
-    $az = Connect-AzAccount -Identity
-  }      
-}  
-#endregion
-$WriteLock = Get-AutomationVariable -Name 'WriteLock'
+  $az = Connect-AzAccount -TenantId 84d7ef22-1ddc-48ce-bf9b-0f099c1ebdf8 -Subscription 6933d5e6-880a-4d60-a474-35b1816d0d62 # Swisscom Azure Testlab Holger
+  }
+else {
+  $az = Connect-AzAccount -Identity
+  }
+}
 
+$PAServer  = Get-AutomationVariable -Name 'PAServer'
+$WriteLock = Get-AutomationVariable -Name 'WriteLock'
+#endregion
+
+Write-Output "output before while writelock "
 $i = 0
 while ( $WriteLock -eq $true -and $i -lt 3 ) {
   $i++
   Write-Output "Currently no write access is allowed ($i/3)"
-    $WaitPeriod = Get-Random -Minimum 30 -Maximum 90
-    Write-Output "Wait for $WaitPeriod seconds and try again"
-    Start-Sleep -Seconds $WaitPeriod
-    $WriteLock = Get-AutomationVariable -Name 'WriteLock'
-  }
+  $WaitPeriod = Get-Random -Minimum 30 -Maximum 90
+  Write-Output "Wait for $WaitPeriod seconds and try again"
+  Start-Sleep -Seconds $WaitPeriod
+  $WriteLock = Get-AutomationVariable -Name 'WriteLock'
+}
 
 if ( $WriteLock -eq $true ) {
   Write-Output "Cannot get write access to the blob container file. Check if another process has crashed!"
@@ -64,12 +63,19 @@ if ( $WriteLock -eq $true ) {
 }
 
 Set-AutomationVariable -Name 'WriteLock' -Value $true
+Write-Output "output behind Set-AutomationVariable " 
+Write-Debug "debug behind Set-AutomationVariable"
+Write-Warning "warning behind Set-AutomationVariable"
 
 try {
-  $s_acc_obj=Get-AzStorageAccount -ResourceGroupName $acme_rg
+  $s_acc_obj = Get-AzStorageAccount -ResourceGroupName $acme_rg
   if (($s_acc_obj).Count -gt 1) { throw("More than one storage account in $acme_rg detected - cannot handle this - exiting.")}
-  $kv_obj = (Get-AzKeyVault -ResourceGroupName $acme_rg)
+  $kv_obj = (Get-AzKeyVault -ResourceGroupName $acme_rg -WarningAction SilentlyContinue)
   if (($kv_obj).Count -gt 1) { throw("More than one keyvault in $acme_rg detected - cannot handle this - exiting.")}
+  $kv_name = (Get-AzKeyVault -ResourceGroupName $acme_rg -WarningAction SilentlyContinue).VaultName
+  if (($kv_name).Count -gt 1) { throw("More than one keyvault in $acme_rg detected - cannot handle this - exiting.")}
+  $kv_name = "acme-kv01"
+  Write-Output "output behind kb_object count " 
   $null = Set-AzCurrentStorageAccount -ResourceGroupName $acme_rg -Name $s_acc_obj.StorageAccountName
   $null = Get-AzStorageBlobContent -Container $poshcontainer -Blob $poshfile -Destination "."
   Expand-Archive $poshfile -DestinationPath .
@@ -79,24 +85,33 @@ try {
   Set-PAServer $PAServer
 
   for ($i = 0; $i -lt $domains.Count; $i++) {
-    "Listing certificate for " + $domains[$i]
+    if ($domains[$i] -match '[^a-zA-Z0-9-.]') { throw("$domains[$i] contains an illegal character.") }
+    "Checking certificate for " + $domains[$i]
     $cert = Get-PACertificate $domains[$i]
     if ( -not ( $cert ) ) { # no certificate found, so lets order one
       $pfxpwd = (-join([char[]](33..122) | Get-Random -Count 30))
+      "before subscription stuff"
       $pArgs = @{
-          AZSubscriptionId = $az.Context.Subscription.Id
-          AZAccessToken = (Get-AzAccessToken -ResourceUrl "https://management.core.windows.net/" -TenantId $az.Context.Tenant ).Token
+        AZSubscriptionId = $az.Context.Subscription.Id
+        AZAccessToken = (Get-AzAccessToken -ResourceUrl "https://management.core.windows.net/" -TenantId $az.Context.Tenant ).Token
       }
+      "after subscription stuff"
       $cert = New-PACertificate $domains[$i] -DnsAlias $ACMEDNSDomain -PfxPass $pfxpwd -Plugin Azure -PluginArgs $pArgs -AcceptTOS -Force # FIXME force raus
-      "Created new certificate : $cert"
+      if ($cert) { "Created new certificate : $cert" } else { throw "New-PACertificate did not fail, but variable cert is empty - ERROR" }
     }
     else {
-      $cert = Submit-Renewal $domains[$i] -Force # FIX force raus
-      "Renewed certificate : $cert"
+      $cert = Submit-Renewal $domains[$i] # -Force # FIXME force raus, autom. var. PAServer noch auf prod
+      "after submit-renewal"
+      if ($cert) { "Renewed certificate : $cert" } else { "No renewal done." }
     }
-    $domain = $domains[$i] -replace ('\.',"") -replace ('[^a-zA-Z0-9-]', '')
-    $null = Import-AzKeyVaultCertificate -VaultName $kv_obj.VaultName -Name $domain -FilePath $cert.PfxFile -Password $cert.PfxPass
+    "$cert is " + $cert + $cert.PfxFile
+    if ($cert) {
+      $certname = $domains[$i] -replace ('\.',"") -replace ('[^a-zA-Z0-9-]', '')
+      "Uploading cert $certname to keyvault $kv_obj.VaultName"
+      $null = Import-AzKeyVaultCertificate -VaultName $kv_obj.VaultName -Name $certname -FilePath $cert.PfxFile -Password $cert.PfxPass
+      $null = Import-AzKeyVaultCertificate -VaultName $kv_name -Name $certname -FilePath $cert.PfxFile -Password $cert.PfxPass
     }
+  }
 }
 finally {
   $null = Compress-Archive -Path $workingDirectory -DestinationPath $env:TEMP\$poshfile -CompressionLevel Fastest -Force
@@ -107,27 +122,21 @@ Return
 
 
 
-
-
 #test-run of this runbook in powershell, you must be in the directoy where this script - file resides
+Get-Date; $ErrorActionPreference = 'Stop' ; $rbname = "order_or_renew_certificate_using_acme"; $acme_rg = "acme-rg01"; $automacc = "acme-atm01"; $domains = @("www5.azure.vankoll.ch","www6.azure.vankoll.ch"); $ACMEDNSDomain = "acme.vankoll.ch"; $rbparams = @{"domains" = $domains; "acme_rg" = $acme_rg; "ACMEDNSDomain" = $ACMEDNSDomain};
+$null = Import-AzAutomationRunbook -Name $rbname -Path .\order_or_renew_certificate_using_acme.ps1 -ResourceGroup $acme_rg -AutomationAccountName $automacc -Type PowerShell -Force -Published
+$rb_out = Start-AzAutomationRunbook -Name $rbname -ResourceGroupName $acme_rg -AutomationAccountName $automacc -Parameters $rbparams
+do { $x = (Get-AzAutomationJob -Id $rb_out.JobId.Guid -ResourceGroupName $acme_rg -AutomationAccountName $automacc) ; Start-Sleep -Seconds 10 } until ( $x.Status.Equals("Failed") -OR $x.Status.Equals("Completed") )
+$jobout = Get-AzAutomationJobOutput -Id $rb_out.JobId -ResourceGroupName $acme_rg -AutomationAccountName $automacc -Stream Any
+$joboutput_sum=$jobout.Summary
+$joboutput = ($jobout | Get-AzAutomationJobOutputRecord).value
+if ( $null -ne $joboutput_sum ) { "joboutput_sum is : " ; $joboutput_sum } ; if ( $null -ne $joboutput.Values ) { Write-Host -NoNewline -ForegroundColor Red -BackgroundColor Yellow "Error details in $env:TEMP\jobout_exception.txt" ; Out-File -InputObject $joboutput.Values -FilePath $env:TEMP\jobout_exception.txt -ErrorAction SilentlyContinue }
 
-Get-Date; $ErrorActionPreference = 'Stop' ; $rbname = "order_or_renew_certificate_using_acme"; $acme_rg = "acme-rg01"; $automacc = "acme-atm01"; $domains = @("www3.azure.vankoll.ch","www4.azure.vankoll.ch"); $ACMEDNSDomain = "acme.vankoll.ch"; $rbparams = @{"domains" = $domains; "acme_rg" = $acme_rg; "ACMEDNSDomain" = $ACMEDNSDomain}; 
-switch ((Get-AzContext).Name) {
-  { $_.contains("6933d5e6-880a-4d60-a474-35b1816d0d62") }   # Azure Testlab Holger
-    {
-      "Running on Azure Testlab (Holger)"
-      $null = Import-AzAutomationRunbook -Name $rbname -Path .\order_or_renew_certificate_using_acme.ps1 -ResourceGroup $acme_rg -AutomationAccountName $automacc -Type PowerShell -Force -Published
-      $rb_out = Start-AzAutomationRunbook -Name $rbname -ResourceGroupName $acme_rg -AutomationAccountName $automacc -Parameters $rbparams
-      do { $x = (Get-AzAutomationJob -Id $rb_out.JobId.Guid -ResourceGroupName $acme_rg -AutomationAccountName $automacc) ; $x.Status; Start-Sleep -Seconds 5 } until ( $x.Status.Equals("Failed") -OR $x.Status.Equals("Completed") )
-      $jobout = Get-AzAutomationJobOutput -Id $rb_out.JobId -ResourceGroupName $acme_rg -AutomationAccountName $automacc -Stream Any
-      $joboutput_sum=$jobout.Summary
-      $joboutput = ($jobout | Get-AzAutomationJobOutputRecord).value
-      if ( $null -ne $joboutput_sum ) { "joboutput_sum is : " ; $joboutput_sum } ; if ( $null -ne $joboutput.Values ) { Write-Host -NoNewline -ForegroundColor Red -BackgroundColor Yellow "Error details in $env:TEMP\jobout_exception.txt" ; Out-File -InputObject $joboutput.Values -FilePath $env:TEMP\jobout_exception.txt -ErrorAction SilentlyContinue }
-    }
-  Default {"Illegal subsciption - set it correctly with Set-AzContext" ; Return}
-  }
+if (((Get-Date) -  (Get-ChildItem C:\Users\holge\AppData\Local\Temp\jobout_exception.txt).LastWriteTime).totalminutes -gt 2 ) { Write-Host -NoNewline -ForegroundColor Red -BackgroundColor Cyan "NO NEW EXCEPTION OUTPUT" }
 # Get-Content -Tail 30 C:\Users\holge\AppData\Local\Temp\jobout_exception.txt
-
+(Get-ChildItem C:\Users\holge\AppData\Local\Temp\jobout_exception.txt)|Select-Object Length,Name,LastWriteTime
+[console]::beep(500,800)
+  
 Return
 
 
@@ -148,3 +157,5 @@ Return
 # PfxPass       NoteProperty System.Security.SecureString PfxPass=System.Security.SecureString
 # Subject       NoteProperty string Subject=CN=www3.vankoll.ch
 # Thumbprint    NoteProperty string Thumbprint=9AADD2891C4EF0CB65CA33122BB010E69508548B
+
+# "daysAfterCreationGreaterThan": 10 # mal prüfen ob der alle Versionen löscht, auch die aktuelle (wenn 10 Tage alt); sollte nicht passieren. The baseBlob element in a lifecycle management policy refers to the current version of a blob. The version element refers to a previous version.
